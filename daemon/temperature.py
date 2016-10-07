8dbecda9c97bcfed3889b4eaa065017578577487
Dima Karacheban <d.karacheban@gmail.com>
2016-10-07 21:22:31 +0300

    Files cleanup

--- daemon/temperature.py
+++ daemon/temperature.py
@@ -14,7 +14,7 @@ while True:
         h, t = dht.read_retry(dht.DHT11, 4)
         cur_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 #        print "%s Temperature: %f*C Humidity: %f%%" % (str(cur_time), t, h)
-        with open('/var/data/dima_weather.txt', 'a+t') as history:
+        with open('/opt/web-meteostation/dima_weather.txt', 'a+t') as history:
             history.write("%s Temperature: %.2f*C Humidity: %.2f%%\n" % (str(cur_time), t, h))
     except Exception as ex:
         print ex
