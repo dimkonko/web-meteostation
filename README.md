951ba31c4b0d026a74b5bdb7518b44c58676729c
Dima Karacheban <d.karacheban@gmail.com>
2016-10-09 13:12:57 +0300

    Finish daemon files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # web-meteostation
+
+### Daemon installation:
+* Check if daemon scripts are executable. If not - `$ sudo chmod 755` for `.sh` and `.py` scripts.
+* Copy the `temperature.sh` script to ``/etc/init.d/` folder with `$ sudo cp temperature.sh /etc/init.d/`
+* Add the symb. links `sudo update-rc.d temperature.sh defaults`
+* Start the daemon `sudo service temperature start`
--- daemon/temperature.py
+++ daemon/temperature.py
@@ -1,24 +1,80 @@
 #!/usr/bin/python
 
-import Adafruit_DHT as dht
-import time
 import datetime
+import time
+import urllib
+import urllib2
+
+#import Adafruit_DHT as dht
+
+
+def enable_logging():
+    import logging.handlers
+    import sys
+
+    # Deafults
+    LOG_FILENAME = "/tmp/temperature.log"
+    LOG_LEVEL = logging.ERROR  # Could be e.g. "DEBUG" or "WARNING"
+
+    # Give the logger a unique name (good practice)
+    logger = logging.getLogger(__name__)
+    # Set the log level to LOG_LEVEL
+    logger.setLevel(LOG_LEVEL)
+    # Make a handler that writes to a file, making a new file at midnight and keeping 3 backups
+    handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when="midnight", backupCount=3)
+    # Format each log message like this
+    formatter = logging.Formatter('%(asctime)s %(levelname)-2s %(message)s')
+    # Attach the formatter to the handler
+    handler.setFormatter(formatter)
+    # Attach the handler to the logger
+    logger.addHandler(handler)
+
+    # Make a class we can use to capture stdout and sterr in the log
+    class MyLogger(object):
+        def __init__(self, loggerObj, level):
+            """Needs a logger and a logger level."""
+            self.logger = loggerObj
+            self.level = level
+
+        def write(self, message):
+            # Only log if there is a message (not just a new line)
+            if message.rstrip() != "":
+                self.logger.log(self.level, message.rstrip())
+
+    # Replace stdout with logging to file at INFO level
+    sys.stdout = MyLogger(logger, logging.INFO)
+    # Replace stderr with logging to file at ERROR level
+    sys.stderr = MyLogger(logger, logging.ERROR)
+
+    return logger
 
 
 DELAY_SECONDS = 10
+logger = enable_logging()
+
+
+def send_request(t, h):
+    url = 'http://localhost:5000/add_data'
+    values = {
+        't': t,
+        'h': h
+    }
+    logger.info('Sending data to %s with values: %s' % (url, str(values)))
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data)
+    # TODO: Handle response data?
+    # response = urllib2.urlopen(req).read()
+    urllib2.urlopen(req).read()
 
 
 while True:
     try:
-        h, t = dht.read_retry(dht.DHT11, 4)
+        h = 33
+        t = 17
+        #h, t = dht.read_retry(dht.DHT11, 4)
         cur_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#        print "%s Temperature: %f*C Humidity: %f%%" % (str(cur_time), t, h)
-        with open('/opt/web-meteostation/dima_weather.txt', 'a+t') as history:
-            history.write("%s Temperature: %.2f*C Humidity: %.2f%%\n" % (str(cur_time), t, h))
+        send_request(t, h)
     except Exception as ex:
-        print ex
+        logger.error(ex.message)
     finally:
         time.sleep(DELAY_SECONDS)
-
-
-
