c233370470d7d8e6f5a927d7e763199ac984ea47
Dima Karacheban <d.karacheban@gmail.com>
2016-10-14 10:34:11 +0300

    Add check to insert only changed data

--- server/src/app.py
+++ server/src/app.py
@@ -11,12 +11,12 @@ from flask import (
 
 from db_api import MySQLApi
 
-
 app = Flask(__name__)
 config_provider.init_config(app)
 
-
 mysqlapi = MySQLApi(config=app.config['DB'])
+
+last_insert = None
 
 
 @app.route('/')
@@ -30,5 +30,24 @@ def add_data():
     t = request.form['t']
     h = request.form['h']
     create_date = request.form['create_date']
-    mysqlapi.insert_meteo_data(t, h, create_date)
+
+    # Insert data only if it's different from previous
+    global last_insert
+    if not last_insert or is_data_changed(t, h, create_date):
+        print "insert"
+        mysqlapi.insert_meteo_data(t, h, create_date)
+
+    last_insert = {
+        't': t,
+        'h': h,
+        'create_date': create_date
+    }
+    print last_insert
     return 'ok'
+
+
+def is_data_changed(t, h, create_date):
+    data_changed = True
+    if t == last_insert['t'] and h == last_insert['h']:
+        data_changed = False
+    return data_changed
